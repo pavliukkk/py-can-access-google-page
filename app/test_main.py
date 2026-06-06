2ebb97b55b04bedc8531771f2f79cce8a81a8d6b
Petro <petropavliuk12.05@gmail.com>
2026-06-06 19:57:33 +0300

    fixed flake8 errors

--- app/test_main.py
+++ app/test_main.py
@@ -2,33 +2,33 @@ from unittest import mock
 from app.main import can_access_google_page
 
 
-@mock.patch('app.main.valid_google_url', return_value = True)
-@mock.patch('app.main.has_internet_connection', return_value = True)
+@mock.patch("app.main.valid_google_url", return_value=True)
+@mock.patch("app.main.has_internet_connection", return_value=True)
 def test_has_connection_and_valid_url(
-    mock_url, mock_internet_connection
-):
+    mock_url: bool, mock_internet_connection: bool
+) -> None:
     assert can_access_google_page("https://www.google.com") == "Accessible"
 
 
-@mock.patch('app.main.valid_google_url', return_value = True)
-@mock.patch('app.main.has_internet_connection', return_value = False)
+@mock.patch("app.main.valid_google_url", return_value=True)
+@mock.patch("app.main.has_internet_connection", return_value=False)
 def test_cannot_access_if_only_valid_url(
-    mock_url, mock_internet_connection
-):
+    mock_url: bool, mock_internet_connection: bool
+) -> None:
     assert can_access_google_page("https://www.google.com") == "Not accessible"
 
 
-@mock.patch('app.main.valid_google_url', return_value = False)
-@mock.patch('app.main.has_internet_connection', return_value = True)
+@mock.patch("app.main.valid_google_url", return_value=False)
+@mock.patch("app.main.has_internet_connection", return_value=True)
 def test_cannot_access_if_only_connection(
-    mock_url, mock_internet_connection
-):
+    mock_url: bool, mock_internet_connection: bool
+) -> None:
     assert can_access_google_page("https://www.google.com") == "Not accessible"
 
 
-@mock.patch('app.main.valid_google_url', return_value = False)
-@mock.patch('app.main.has_internet_connection', return_value = False)
+@mock.patch("app.main.valid_google_url", return_value=False)
+@mock.patch("app.main.has_internet_connection", return_value=False)
 def test_has_connection_and_valid_url_are_false(
-    mock_url, mock_internet_connection
-):
+    mock_url: bool, mock_internet_connection: bool
+) -> None:
     assert can_access_google_page("https://www.google.com") == "Not accessible"
